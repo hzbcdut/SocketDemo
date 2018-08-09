00cc9de0f2318344c37c188964c6879e64d71753
hans <hzhengbiao@126.com>
2018-08-09 13:44:05 +0800

    1

--- app/src/main/java/com/hivebox/socketdemo/MainActivity.java
+++ app/src/main/java/com/hivebox/socketdemo/MainActivity.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import okio.ByteString;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ServerActivity.class);
-                startActivity(intent);
+//                startActivity(intent);
+
+                testOkio();
             }
         });
 
@@ -45,6 +52,18 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
         } else {
             // todo 已经有此权限了，可以做点什么
+        }
+    }
+
+    public void testOkio() {
+        ByteString byteString = ByteString.decodeHex("89504e470d0a1a0a");
+        String hex = byteString.hex();
+        int size = byteString.size();
+        Log.i("debug", TAG + " --> byteString = " + byteString + " hex = " + hex + "  size = " + size);
+
+        for (int i = 0; i < byteString.size(); i ++) {
+            Log.i("debug", TAG + " 字节  i = " + i + "  byte = " + byteString.getByte(i));
+
         }
     }
 }
